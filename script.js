df44d96c31a4bdd6062fad6c0bebb0bb30bb31fc
Kaigagi <kaigagipro@gmail.com>
2021-04-24 15:48:53 +0700

    finish time

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-let dateShow = document.getElementById("date")
-// console.log(dateShow)
+// let dateShow = document.getElementById("date")
+// // console.log(dateShow)
+// let dateBegin = new Date(2018,10,21,23,59,59,59).getTime()
+// dateShow.innerHTML = "Love Time"
+// let dateNow = Date.now()
+// // console.log(dateBegin)
+// // console.log(dateNow)
+// let Time = dateNow - dateBegin
+// // console.log(Time)
+// let parseDays = Math.floor(Time/(1000*60*60*24))
+// let parseHours = Math.floor((Time-parseDays*(1000*60*60*24))/(1000*60*60))
+// let parseMinutes = Math.floor((Time-parseDays*(1000*60*60*24)-parseHours*(1000*60*60))/(1000*60))
+// let parseSeconds = Math.floor((Time-parseDays*(1000*60*60*24)-parseHours*(1000*60*60)-parseMinutes*(1000*60))/(1000))
+// console.log(parseDays)
 let dateBegin = new Date(2018,10,21,23,59,59,59).getTime()
-dateShow.innerHTML = "Love Time"
-let dateNow = Date.now()
-// console.log(dateBegin)
-// console.log(dateNow)
-let Time = dateNow - dateBegin
-// console.log(Time)
-let parseDays = Math.floor(Time/(1000*60*60*24))
-let parseHours = Math.floor((Time-parseDays*(1000*60*60*24))/(1000*60*60))
-let parseMinutes = Math.floor((Time-parseDays*(1000*60*60*24)-parseHours*(1000*60*60))/(1000*60))
-let parseSeconds = Math.floor((Time-parseDays*(1000*60*60*24)-parseHours*(1000*60*60)-parseMinutes*(1000*60))/(1000))
-console.log(parseDays)
 function updateDate() {
+    let dateShow = document.getElementById("date")
+    // console.log(dateShow)
+    let dateNow = Date.now()
+    // console.log(dateBegin)
+    // console.log(dateNow)
+    let Time = dateNow - dateBegin
+    // console.log(Time)
+    let parseDays = Math.floor(Time/(1000*60*60*24))
+    let parseHours = Math.floor((Time-parseDays*(1000*60*60*24))/(1000*60*60))
+    let parseMinutes = Math.floor((Time-parseDays*(1000*60*60*24)-parseHours*(1000*60*60))/(1000*60))
+    let parseSeconds = Math.floor((Time-parseDays*(1000*60*60*24)-parseHours*(1000*60*60)-parseMinutes*(1000*60))/(1000))
     dateShow.innerHTML = parseDays +" Days : "+ parseHours+" Hours : "+parseMinutes+" Minutes : "+parseSeconds+" Seconds"
-    console.log(Time)
 }
+setInterval(()=>{
+    updateDate()
+}, 1000);
+ 
